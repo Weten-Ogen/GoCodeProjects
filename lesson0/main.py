0a9bc742c5545ed5eaee5f848c0ad89b95a47bb4
Weten-Ogen <alphaoware@gmail.com>
2024-06-26 15:25:42 +0000

    created folders for studinets

--- lesson0/main.py
+++ lesson0/main.py
@@ -1,7 +1,7 @@
 # this is a tryout 
 
 print('hello fire')
-<<<<<<< HEAD
+
 # pull request 
 # git log 
 # git status
@@ -10,6 +10,3 @@ print('hello fire')
 # git push or (git pull... git fetch )pull request
 # staging area ,commiting, merging 
 # 
-=======
-
->>>>>>> 76be387fd6c8ae3dbdde431a18c4df4ecd78db97
